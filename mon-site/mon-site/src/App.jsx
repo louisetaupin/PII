4fa2f4a03f7c319ccea917175386f207df40b35f
Louise Taupin <ltaupin@ensc.fr>
2025-02-23 11:50:29 +0100

    la page du site s'affiche

--- mon-site/mon-site/src/App.jsx
+++ mon-site/mon-site/src/App.jsx
@@ -1,15 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Home from "./pages/Home.jsx";
+import Projects from "./pages/Projects.jsx";
+import Contact from "./pages/Contact.jsx";
+import Footer from "./components/Footer.jsx";
+
+console.log("Navbar:", Navbar);
+console.log("Home:", Home);
+console.log("Projects:", Projects);
+console.log("Contact:", Contact);
+console.log("Footer:", Footer);
 
 function App() {
   return (
-    <Router>
-      {<div className="text-xs">hello</div>/* <Navbar />
+    <Router> {/* 🚀 Ajout du Router ici */}
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-      <Footer /> */}
+      <Footer />
     </Router>
   );
 }
---
+++ mon-site/mon-site/src/components/Footer.jsx
@@ -0,0 +1,6 @@
+function Footer() {
+    return <footer>Mon pied de page</footer>;
+  }
+  
+  export default Footer; // ✅ Exportation correcte
+  
